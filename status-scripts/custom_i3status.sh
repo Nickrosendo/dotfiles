acb91cf3afb69ca8abd6e96919a55330c5a5803c
Nicolas Rosendo <nicolasrosendo@hotmail.com>
2020-11-23 12:37:06 +0000

    :sparkles: include status-bar script

---
+++ status-scripts/custom_i3status.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# shell script to prepend i3status with more stuff
+
+temperature=""
+get_cpu_temperature() {
+  temperature="cpu_temperature: $(sensors | grep Tctl: | awk '{ print $2 $3}')"
+}
+
+i3status | (read line && echo "$line" && read line && echo "$line" && read line && echo "$line" && get_cpu_temperature && while :
+do
+  read line
+  get_cpu_temperature 
+  echo ",[{\"full_text\":\"${temperature}\" },${line#,\[}" || exit 1 
+done)
