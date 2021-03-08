065e5af657b34145cee9f873e2f1c23252ab5724
Nicolas Rosendo <nicolasrosendo@hotmail.com>
2021-03-08 03:04:07 -0400

    :wrench: update config files from xmonad and alacritty

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -257,7 +257,7 @@ myStartupHook =	do
 -- Run xmonad with the settings you specify. No need to modify this.
 --
 main = do
-  xmproc <- spawnPipe "xmobar -x 0 ~/.xmobarrc"
+  xmproc <- spawnPipe "xmobar -x 0 ~/.config/xmobar/xmobar.config"
   xmonad $ docks defaults
 
 -- A structure containing your configuration settings, overriding
